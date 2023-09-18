7f8ea780962d2755b401dff54b35b2e5e633682a
Ebrahim Moeini <ebi3102@gmail.com>
2023-09-18 21:07:14 +0330

    Create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+'use client';
+
+// import Image from "next/image";
+// import Main_Menu from "./headers/main-menu";
+
+const Header = () => {
+    return ( 
+        <header className="emo-header flex text-white bg-[#2b549e] items-center justify-between p-4">
+            Hello Menu
+        </header>
+     );
+}
+ 
+export default Header;
